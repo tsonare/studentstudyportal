5cd8aa867b30b6a69825d55c375f6326efd83e11
tsonare <tsonare@bestpeers.com>
2022-02-24 17:42:18 +0530

    completed notes section.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name = "home"),   
+    path('', views.home_authenticated, name = "home_authenticated"),   
+    path('home_not_authenticated', views.home_not_authenticated, name = "home_not_authenticated"),   
     path('notes', views.notes, name = "notes"),    
     path('delete_note/<int:pk>', views.delete_note, name = "delete_note"),    
     path('notes_detail/<int:pk>', views.NotesDetailView.as_view(), name = "notes_detail"),
+    path('edit_note/<int:pk>', views.edit_note, name = "edit_note"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,8 +7,12 @@ from django.views.generic import DetailView
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'dashboard/home.html')
+def home_authenticated(request):
+    return render(request, 'dashboard/home_authenticated.html')
+
+
+def home_not_authenticated(request):
+    return render(request, 'dashboard/home_not_authenticated.html')
 
 
 def notes(request):
@@ -18,7 +22,7 @@ def notes(request):
             notes = Note(user=request.user,title=request.POST['title'],description=request.POST['description'])
             notes.save()
         messages.success(request, f"Note saved from {request.user.username} successfully.")
-
+        return redirect('notes')
     else:
         form = Notesform()
     notes = Note.objects.filter(user=request.user)
@@ -28,11 +32,25 @@ def notes(request):
 
 def delete_note(request,pk=None):
     Note.objects.get(id=pk).delete()
-    return redirect("notes")
+    messages.success(request, f"Note Deleted from {request.user.username} successfully.")
+    return redirect('notes')
 
 
 class NotesDetailView(DetailView):
     model = Note
     template_name = 'dashboard/notes_detail.html'
+
+
+def edit_note(request,pk):
+    notes = Note.objects.get(id=pk)
+    form = Notesform(instance=notes)
+    if request.method == "POST":
+        form = Notesform(request.POST, instance=notes)
+        if form.is_valid():
+            form.save()
+        messages.success(request, f"Updated Note saved from {request.user.username} successfully.")
+        return redirect('notes')
+    context = {'form':form,}
+    return render(request, 'dashboard/edit_note.html', context)
 
 
